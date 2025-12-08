d563ac5615a6ff39674f1e1a8f392a40c454c54e
noah.nam <noah.nam@kakaovx.com>
2025-12-08 14:11:38 +0900

    Deploy: update web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2231321737"
+    serviceWorkerVersion: "384616722"
   }
 });
