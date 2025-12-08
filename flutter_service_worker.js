f5c554d8e13cf77a193e71594730a5e12da62f28
noah.nam <noah.nam@kakaovx.com>
2025-12-08 14:06:13 +0900

    Deploy: update web build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "df0db9d07c76bfe7ea50596cf849b3a7
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "30d3a63c48acb0ee7b6425dbd384e310",
+"flutter_bootstrap.js": "64bc45ee97b4a40f70622f5fa5e573c0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
